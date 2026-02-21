a451be7e307489cca60834e2f21010c79ef5ccb3
Caesar Moussalli <cesar_moussalli@hotmail.com>
2026-02-21 10:38:15 +0400

    Project Init

---
+++ DocQL/Program.cs
@@ -0,0 +1,48 @@
+using DocQL.Components;
+using DocQL.Services;
+
+namespace DocQL
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            // Register DocQL services (scoped = per-circuit/per-tab)
+            builder.Services.AddScoped<ConnectionManager>();
+            builder.Services.AddScoped<QueryExecutionService>();
+            builder.Services.AddScoped<SchemaDiscoveryService>();
+            builder.Services.AddScoped<ScriptGeneratorService>();
+            builder.Services.AddScoped<BackupRestoreService>();
+            builder.Services.AddScoped<SecurityService>();
+            builder.Services.AddScoped<MonitoringService>();
+            builder.Services.AddScoped<AgentJobService>();
+            builder.Services.AddScoped<ImportExportService>();
+            builder.Services.AddScoped<MaintenanceService>();
+            builder.Services.AddScoped<IntelliSenseService>();
+            builder.Services.AddSingleton<TemplateService>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+            app.UseAntiforgery();
+
+            app.MapStaticAssets();
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
